e7d8a517d4b2732964e43c43ff7525931382b2e8
Ashley Templet <ashley.templet@gmail.com>
2017-04-18 15:07:20 -0400

    Back to Basics

---
+++ PFKCh3.py
@@ -0,0 +1,15 @@
+hobbies = ['video games', 'reading', 'cross-stitch', 'learning new stuff']
+food = ['steak', 'pizza', 'chicken']
+favorites = hobbies + food
+print (favorites)
+
+buildings = 3
+ninjas = 25
+tunnels = 2 
+samurai = 40
+
+print (buildings * ninjas) + (tunnels * samurai)
+
+firstName = 'Ashley'
+lastName = 'Templet'
+print (' Hi there, %s %s!' %(firstName, lastName))
